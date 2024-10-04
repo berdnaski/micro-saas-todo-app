c7f9d686ab2911fb965d9bd77c3be65a82e28ffa
berdnaski <berdnaski1177@gmail.com>
2024-10-04 13:33:57 -0300

    chore: initial configuration

---
+++ src/app/app/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return (
+        <h1>App Page</h1>
+    )
+}
---
+++ src/app/auth/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return (
+        <h1>Auth Page</h1>
+    )
+}
